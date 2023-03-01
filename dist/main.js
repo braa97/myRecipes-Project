8bce3a8b72972a9db0edc0427a2a50c7b19e4304
Braa Knaane <braa97@windowslive.com>
2023-03-02 00:37:40 +0200

    - made a function to display the recipes from the api - css and html are finished except for some minor changes * to do - add checkbox and filter feature

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,13 @@
+let renderer = new Renderer()
 $("#Ingredient-search-button").on("click", function() {
     let ingredient = $(this).siblings("#ingredientInputField").val()
-    $.get('/ingredients', function(response) {
-        
+
+    $.get(`/recipe/${ingredient}`).then((data) => {
+        renderer.render(".recipe-album-container", "#ingredientContainerTemplate", data)
     })
+})
+
+$(".recipe-album-container").on("click", "img", function() {
+    let firstIngredient = $(this).closest(".recipe-container").find("ul").find("li:first").text()
+    alert(firstIngredient)
 })
--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const axios = require('axios')
 const api = require('./server/routes/api')
+const { data } = require('jquery')
+const INGREDIENT_URL = 'https://recipes-goodness-elevation.herokuapp.com/recipes/ingredient'
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
-app.use("/", api);
+
+app.use('/', api)
+
+app.use('/recipe/:ingredient', function(req, res) {
+    let ingredient = req.params.ingredient
+    axios.get(`${INGREDIENT_URL}/${ingredient}`)
+    .then((recipes) => {
+        res.send(
+            recipes.data.results.map((e) => {
+                return {
+                    id: e.idMeal,
+                    title: e.title,
+                    image: e.thumbnail,
+                    link: e.href,
+                    ingredients: e.ingredients
+                }
+            })
+        )
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+
 
 const port = 3000
 app.listen(port, function () {
     console.log(`Server running on http://localhost:${port}`)
 })
+
+module.exports.INGREDIENT_URL = INGREDIENT_URL
